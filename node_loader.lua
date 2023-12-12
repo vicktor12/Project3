30e06a75aa9b2c392b8addb7a6807f9e3ff842c1
vicktor12 <victorsouza0@icloud.com>
2023-12-12 14:45:47 -0300

    Criado função recursiva para carregar todos os nodes

--- node_loader.lua
+++ node_loader.lua
@@ -4,6 +4,8 @@ local nodeDictionary  = {} ---@type table<string,Node>
 local initialNode = nil ---@type Node
 local hasErrors = false
 
+---@param path string
+---@return Node
 local function loadNode(path)
     local success, nodeOrErr = pcall(function ()
         return require(path)   
@@ -11,25 +13,29 @@ local function loadNode(path)
     if not success then
         warn("falha ao carregar o node ".. path .. ". O arquivo não foi encontrado")
         hasErrors = true
-        return
+        return nil
     end
     local node = nodeOrErr
     if nodeDictionary[node.id] ~= nil then
         warn("falha ao carregar o node ".. path .. ". O ID " .. node.id .. " já está registrado.")
         hasErrors = true
-        return
+        return nil
     end
     nodeDictionary[node.id] = node
+    return node
 end
+
 
 function nodeLoader.loadnodes()
     nodeDictionary = {}
     initialNode = require("nodes.start")
     nodeDictionary[initialNode] = initialNode
 
-    loadNode("nodes.andes.start")
-    loadNode("nodes.andes.surprise")
-    loadNode("nodes.wahalla.start")
+    nodeLoader.loadNodesFromChoices(initialNode)
+
+    -- loadNode("nodes.andes.start")
+    -- loadNode("nodes.andes.surprise")
+    -- loadNode("nodes.wahalla.start")
 
     for id, node in pairs(nodeDictionary) do
         for _, choice in pairs(node.choices) do
@@ -37,6 +43,19 @@ function nodeLoader.loadnodes()
             local destinationNode = nodeDictionary[destinationId]
             if destinationNode == nil then
                 warn("Uma das escolhas do "..node.id..", tem como destino um node que não existe. "..destinationId)
+            end
+        end
+    end
+end
+
+---@param parentNode Node
+function nodeLoader.loadNodesFromChoices(parentNode)
+    for _, choice in pairs(parentNode.choices) do
+        local destinationId = choice.destination
+        if nodeDictionary[destinationId] then
+            local childNode = loadNode("node."..destinationId)            
+            if childNode then
+                nodeLoader.loadNodesFromChoices(childNode)
             end
         end
     end
